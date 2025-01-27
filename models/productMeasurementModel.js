a1975a779d3cd08a97df710a122c08e7ad17f1e0
Ronik <102733678+Ronikrr@users.noreply.github.com>
2025-01-27 15:49:48 +0530

    ronik10:09

--- models/productMeasurementModel.js
+++ models/productMeasurementModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const measurementSchema = mongoose.Schema({
-    size: {
-        type: String,
-        required: true,
-    },
+    // size: {
+    //     type: String,
+    //     required: true,
+    // },
     age: {
         type: String,
         required: true,
